ddfcccf9e9a3a4cc5c37e3be8a65755a0d9b8027
ChuckleScriptCraft <hxzzmxnch@gmail.com>
2024-12-03 10:17:48 +0000

    03: Part Two

--- 03/day_03.js
+++ 03/day_03.js
@@ -13,3 +13,23 @@ for(let i of matches){
 }
 
 console.log(p1)
+
+// Part Two
+const p2Regex = /mul\(\d{1,3},\d{1,3}\)|do(?:n't)?\(\)/g;
+const p2Matches = data.match(p2Regex)
+
+let p2 = 0, enable = true
+for(let i of p2Matches){
+    if (i === 'do()'){
+        enable = true
+    } else if (i === "don't()"){
+        enable = false 
+    } else {
+        if (enable){
+        const [x,y] = i.match(/\d+/g).map(Number)
+        p2 += x * y
+        }
+    }
+}
+
+console.log(p2)
