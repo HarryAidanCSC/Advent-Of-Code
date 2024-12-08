3b9ffbb38cedf41bafa194ccbb21c360c2c9b037
ChuckleScriptCraft <hxzzmxnch@gmail.com>
2024-12-08 11:40:27 +0000

    08: Part Two

--- 08/day_08.py
+++ 08/day_08.py
@@ -4,24 +4,35 @@ from pathlib import Path
 with open(Path(__file__).parent / 'input.txt', 'r') as file:
     lines = [list(line.strip()) for line in file]
 
-# Find Node locations
 maxi, maxj = len(lines) - 1, len(lines[0]) - 1
 
-nodes= {}
+nodes = {}
 p1 = set()
+p2 = set()
 
 for i, line in enumerate(lines):
     for j, val in enumerate(line):
         if val != ".": 
-            if nodes.get(val): 
-                matching_nodes = nodes[val]
-                for i2,j2 in matching_nodes:
-                    v1 = (2 * i - i2, 2 * j - j2)
-                    if v1[0] >= 0 and v1[0] <= maxi and v1[1] >= 0 and v1[1] <= maxj:
-                        p1.add(v1)
-                    v2 = (2 * i2 - i, 2 * j2 - j)
-                    if v2[0] >= 0 and v2[0] <= maxi and v2[1] >= 0 and v2[1] <= maxj:
-                        p1.add(v2)
-                nodes[val].append((i,j))
-            else: nodes[val] = [(i,j)]
+            if val in nodes:
+                matching_nodes = nodes[val]               
+                
+                for i2, j2 in matching_nodes:
+                    p2.add((i2, j2))
+                    
+                    for fx in range(-50, 50):
+                        idiff = i - i2
+                        jdiff = j - j2
+                        v1 = (i + (fx * idiff), j + (fx * jdiff))
+                        
+                        if 0 <= v1[0] <= maxi and 0 <= v1[1] <= maxj:
+                            p2.add(v1)
+                            if fx == 1 or fx == -2:
+                                p1.add(v1)
+
+                nodes[val].append((i, j))
+            else:
+                nodes[val] = [(i, j)]
+
+# Output results
 print(len(p1))
+print(len(p2))
