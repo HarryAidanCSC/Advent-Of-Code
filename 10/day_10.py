13051a1a825a146bf9ec47e460c39bf0869f3c56
ChuckleScriptCraft <hxzzmxnch@gmail.com>
2024-12-10 09:58:28 +0000

    Feature: Day 10 Part Two

--- 10/day_10.py
+++ 10/day_10.py
@@ -1,4 +1,9 @@
+# %%
 from pathlib import Path
+from collections import deque
+
+with open(Path(__file__).parent / 'input.txt', 'r') as file:
+    lines = [line.strip() for line in file]
 
 grid = {}
 start = []
@@ -17,18 +22,50 @@ p1 =  0
 for s in start:
     queue.append(s)
     part = set()
-    visited = set()
     while queue:
         i,j = queue.pop()
         val = grid[(i,j)]
-        visited.add((i,j))
         
         if val == 9:
             part.add((i,j))
         
         for x, y in [(-1,0), (0,1), (1,0), (0, -1)]:
-            if grid.get((i + x, j + y)) == val + 1 and (i + x, j + y) not in visited:
+            if grid.get((i + x, j + y)) == val + 1:
                 queue.append((i + x, j + y))
     p1 += len(part)
-              
-print(p1,'\n/n' )
+
+print(p1)
+
+# Part Two
+
+# %%
+
+lines = [
+    '89010123',
+'78121874',
+'87430965',
+'96549874',
+'45678903',
+'32019012',
+'01329801',
+'10456732',
+]
+
+p2 = 0
+for s in start:
+    queue = deque()
+    i, j = s
+    queue.append((i,j))
+    while queue:
+        i,j = queue.pop()
+        value = grid[(i,j)]
+        for x,y in [(1,0), (0,1), (-1,0), (0,-1)]:
+            if grid.get((i + x,j + y)) == value + 1:
+                
+                if grid[(i + x,j + y)] == 9:
+                    p2 += 1
+                else:         
+                    queue.append((i + x,j + y))
+    
+
+print(p2)
