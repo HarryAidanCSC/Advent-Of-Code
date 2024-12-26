2b801c8584861fc98939e030047091d9751425c2
ChuckleScriptCraft <hxzzmxnch@gmail.com>
2024-12-26 14:57:21 +0000

    23: Part 2

--- 23/day_23.py
+++ 23/day_23.py
@@ -1,5 +1,7 @@
+# %%
 from pathlib import Path
 from itertools import combinations
+from collections import defaultdict
 
 with open(Path(__file__).parent.parent / '23/input.txt', 'r') as file:
     lines = set([tuple(line.strip().split("-")) for line in file])
@@ -18,5 +20,32 @@ for group in groups:
     if all(pair in lines or pair[::-1] in lines for pair in pairs):
             p1 += 1
 
-# Part one
+def bron_kerbosch(R, P, X, graph, cliques):
+    if not P and not X:
+        cliques.append(R)
+        return
+    
+    for v in list(P):
+        bron_kerbosch(
+            R.union({v}),
+            P.intersection(graph[v]),
+            X.intersection(graph[v]),
+            graph,
+            cliques
+        )
+        P.remove(v)
+        X.add(v)
+
+graph = defaultdict(list)
+
+for l1, l2 in lines:
+    graph[l1].append(l2)
+    graph[l2].append(l1)
+
+R, P, X = set(), set(graph.keys()), set()
+cliques = []
+bron_kerbosch(R, P, X, graph, cliques)
+
+# P1 and P2
 print(p1)
+print(",".join(sorted(max(cliques, key=len))))
