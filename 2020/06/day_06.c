c140f4409c34ed83e84649bfb32a5d7d1c082ad7
ChuckleScriptCraft <hxzzmxnch@gmail.com>
2025-06-17 22:59:52 +0100

    Day 06 2020 - Part Two

--- 2020/06/day_06.c
+++ 2020/06/day_06.c
@@ -1,37 +1,56 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <ctype.h>
 
 int main(){
     FILE *file = fopen("input.txt", "r");
     int partOne = 0;
+    int partTwo = 0;
     char line[28];
     int scores[26] = {0};
+    int scoresInverse[26] = {0};
 
     while(fgets(line, sizeof(line), file)){
         line[strcspn(line, "\n")] = '\0';
         if (strlen(line) == 0){
-            int groupScore = 0;
+            int groupScorePartOne = 0, groupScorePartTwo = 0;
             for (int j = 0; j < 26; j++)  {
-                groupScore += scores[j];
+                groupScorePartOne += scores[j];
+                groupScorePartTwo += (1 - scoresInverse[j]);
                 scores[j] = 0;
+                scoresInverse[j] = 0;
             }
-            partOne += groupScore;
+            partOne += groupScorePartOne;
+            partTwo += groupScorePartTwo;
             continue;
         } 
 
+        int temp[26] = {0};
         for (int j = 0; j < strlen(line); j++){
             int idx = (int)line[j] - 97;
-            scores[idx] = 1;
+            temp[idx] = 1;
+        }
+        for (int j = 0; j < 26; j++){
+            if (temp[j] == 1){
+                scores[j] = 1;
+                continue;
+            } else {
+                scoresInverse[j] = 1;
+            }
         }
         
     }
+
     // Last group 
-    int groupScore = 0;
-    for (int j = 0; j < 26; j++)  groupScore += scores[j];
-    partOne += groupScore;
+    int groupScorePartOne = 0, groupScorePartTwo = 0;
+    for (int j = 0; j < 26; j++)  {
+        groupScorePartOne += scores[j];
+        groupScorePartTwo += (1 - scoresInverse[j]);
+    }
+    partOne += groupScorePartOne;
+    partTwo += groupScorePartTwo;
 
     printf("\nPart One: %d", partOne);
+    printf("\nPart Two: %d", partTwo);
     return 0;
 }
