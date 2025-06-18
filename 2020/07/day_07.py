fd0826d92b0f9953682e422c395741b81e10202c
ChuckleScriptCraft <hxzzmxnch@gmail.com>
2025-06-18 20:48:32 +0100

    Day 07 2020 - Part Two

--- 2020/07/day_07.py
+++ 2020/07/day_07.py
@@ -1,4 +1,3 @@
-# %%
 import re
 from collections import defaultdict
 
@@ -9,23 +8,39 @@ with open("input.txt", "r") as f:
     ]
 
 containers = defaultdict(list)
+inverseContainer = defaultdict(list)
 
 for line in lines:
     bags = [bag.strip() for bag in line.split(r" contain ")]
     for bag in bags[1].split(", "):
         if bag != "no other":
             containers[re.sub(r"[0-9]", "", bag).strip()].append(bags[0])
+            inverseContainer[bags[0]].append(bag.strip())
 
 visited = set()
 stack = ["shiny gold"]
-
 while stack:
     color = stack.pop()
     visited.add(color)
     next_colors = containers.get(color)
     if next_colors:
         for next_color in next_colors:
-            if next_color not in visited:
-                stack.append(next_color)
+            stack.append(next_color)
+
+
+stack = [(1, "shiny gold")]
+p2 = 0
+while stack:
+    num, color = stack.pop()
+    p2 += num
+    next_colors = inverseContainer.get(color, None)
+    if next_colors:
+        for next_color in next_colors:
+            next_num, col0, col1 = next_color.split(" ")
+            next_num = int(next_num) * num
+            col = col0 + " " + col1
+            stack.append((next_num, col))
+
 
 print(f"Part One: {len(visited) - 1}")
+print(f"Part Two: {p2 - 1}")
