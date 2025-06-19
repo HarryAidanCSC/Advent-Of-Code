b0ae317246f436e3c67fa71dbdf7bdf3cb1c71d8
ChuckleScriptCraft <hxzzmxnch@gmail.com>
2025-06-19 22:46:24 +0100

    Day 09 2020 - Part Two

--- 2020/09/day_09.c
+++ 2020/09/day_09.c
@@ -2,21 +2,28 @@
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
+#include <limits.h>
 
 int main() {
     FILE *file = fopen("input.txt", "r");
-    int partOne = 0;
+    long long int lines[2000];
+    int linesSize = 0;
     char line[20];
     int lineNumeric[25];
     long long int cur;
+    long long int partOne;
     int stacklen = 0;
     int preamble = 25;
 
+    // Part One
     int i = 0;
     while(fgets(line, sizeof(line), file)){
         line[strcspn(line, "\n")] = '\0';
         if (strcmp(line, "\n") == 0) break;
         cur = atoll(line);
+
+        // Chuck into lines for later
+        lines[linesSize++] = cur;
 
         // Checking logic
         // Break out the 0(N^2)
@@ -38,7 +45,7 @@ int main() {
                 }
             }
             if (breakflagMajor == 0) {
-                break;
+                partOne = cur;
             }
         }
 
@@ -51,6 +58,35 @@ int main() {
         }
     }
 
-    printf("\nPart One: %lld", cur);
+
+
+    // Part Two
+    // Variable length sliding window
+    int left = 0;
+    long long int sumOfTerms = 0;
+    long long int partTwo;
+    for (int right = 0; right < linesSize; right++) {
+        cur = lines[right];
+        sumOfTerms += cur;
+
+        // Checking logic
+        while ((sumOfTerms > partOne) && (left < right - 1)){
+            sumOfTerms -= lines[left++];
+        }
+        if (sumOfTerms == partOne){
+            long long int minVal = LLONG_MAX;
+            long long int maxVal = -1;
+            for (int x = left; x <= right; x++){
+                if (lines[x] < minVal) minVal = lines[x];
+                if (lines[x] > maxVal) maxVal = lines[x];
+                partTwo = maxVal + minVal;
+            }
+            printf("I poo %d %d", left, right);
+        } 
+        
+    }
+
+    printf("\nPart One: %lld", partOne);
+    printf("\nPart Two: %lld", partTwo);
     return 0;
 }
