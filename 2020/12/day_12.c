2293e69bcceee8c7f604d99414e04d2a200d82e9
ChuckleScriptCraft <hxzzmxnch@gmail.com>
2025-06-21 13:28:41 +0100

    Day 12 2020 - Part Two

--- 2020/12/day_12.c
+++ 2020/12/day_12.c
@@ -22,13 +22,42 @@ int* newDirection(int num, int d[2], char rorl, int output[2]) {
     return output;
 }
 
+int* waypointRecalibration(int num, int eWay, int nWay, char rorl, int output[2]){
+    num = (num / 90) % 4;
+    if (rorl == 'L') num = (4 - num) % 4;
+
+    int tempEWay = eWay;
+    int tempNWay = nWay;
+
+    if (num == 1){
+        nWay = -tempEWay;
+        eWay = tempNWay;
+    } else if (num == 2){
+        nWay = -tempNWay;
+        eWay = -tempEWay;
+    } else if (num == 3){
+        nWay = tempEWay;
+        eWay = -tempNWay;
+    }
+    
+    output[0] = eWay;
+    output[1] = nWay;
+    return output;
+    
+}
 
 int main(){
     FILE *file = fopen("input.txt", "r");
-    char line[7];
+    char line[5];
     int east = 0;
     int north = 0;
     int dirs[2] = {1, 0};
+    
+    // Part two values
+    int northWaypoint = 1;
+    int eastWayPoint = 10;
+    int northP2 = 0;
+    int eastP2 = 0;
 
     // Temporary vars for direction detection
     char tempDir;
@@ -39,7 +68,6 @@ int main(){
         line[strcspn(line, "\n")] = '\0';
         // Get the D
         tempDir = line[0];
-        printf("%c\n", tempDir);
         // Get a numeric 
         memcpy(tempNum, line + 1, strlen(line));
         int num = atoi(tempNum);
@@ -48,32 +76,49 @@ int main(){
         // Move in direction of east/west/north/south
         if(tempDir == 'N'){
             north += num; 
+            northWaypoint += num;
         }
         else if(tempDir == 'S'){
-            north -= num; 
+            north -= num;
+            northWaypoint -= num; 
         }
         else if(tempDir == 'E'){
             east += num; 
+            eastWayPoint += num; 
         }
         else if(tempDir == 'W'){
             east -= num; 
+            eastWayPoint -= num; 
         } else if (tempDir ==  'F') {
+            // p1
             north += (dirs[1] * num);
             east += (dirs[0] * num);
+
+            // p2
+            northP2 += (num * northWaypoint);
+            eastP2 += (num * eastWayPoint);
         } else if ((tempDir == 'L') || (tempDir == 'R')){
             int newDirs[2];
             newDirection(num, dirs, tempDir, newDirs);
             
-            // Reassign to direction vector
+            // Reassign to direction vector p1
             dirs[0] = newDirs[0];
             dirs[1] = newDirs[1];
+
+            // Part two waypoint rotation
+            int newWaypointVector[2];
+            waypointRecalibration(num, eastWayPoint, northWaypoint, tempDir, newWaypointVector);
+            eastWayPoint = newWaypointVector[0];
+            northWaypoint = newWaypointVector[1];
         }
     }
     
 
     fclose(file);
     int partOne = abs(east) + abs(north) ;
-    printf("Part One: %d (%d + %d)\n", partOne, east, north);
+    int partTwo = abs(eastP2) + abs(northP2);
+    printf("Part One: %d\n", partOne);
+    printf("Part Two: %d\n", partTwo);
     return 0;
 
 
