c6b2508455521496be1beb5509b6f3beda897ddf
Harry <155183070+HarryAidanCSC@users.noreply.github.com>
2025-07-24 09:26:19 +0000

    Day 15 2020 - Part Two

--- 2020/15/day_15.java
+++ 2020/15/day_15.java
@@ -2,23 +2,24 @@ import java.util.HashMap;
 
 public class day_15 {
     public static void main(String[] args) {
-        int[] startingNumbers = { 15, 5, 1, 4, 7, 0 }; // Define input array
-        int lastNumber = startingNumbers[0];
-        HashMap<Integer, Integer> recentShout = new HashMap<>();
+        int[] finalI = { 2020, 30000000 }; // Define the final values to explore
+        for (int y = 0; y < 2; y++) {
+            int[] startingNumbers = { 15, 5, 1, 4, 7, 0 }; // Define input array
+            int lastNumber = startingNumbers[0];
+            HashMap<Integer, Integer> recentShout = new HashMap<>();
 
-        // Initialise default Hashmap values
-        for (int i = 1; i < 2020; i++) {
-            if (i < startingNumbers.length) {
-                recentShout.put(lastNumber, i);
-                lastNumber = startingNumbers[i];
-            } else {
-                // Start main exec
-                Integer mappedValue = recentShout.getOrDefault(lastNumber, i);
-                recentShout.put(lastNumber, i);
-                // System.out.println(lastNumber + " " + i + "-" + mappedValue);
-                lastNumber = i - mappedValue;
+            for (int i = 1; i < finalI[y]; i++) {
+                if (i < startingNumbers.length) {
+                    recentShout.put(lastNumber, i);
+                    lastNumber = startingNumbers[i];
+                } else {
+                    // Start main exec
+                    Integer mappedValue = recentShout.getOrDefault(lastNumber, i);
+                    recentShout.put(lastNumber, i);
+                    lastNumber = i - mappedValue;
+                }
             }
+            System.out.println(lastNumber);
         }
-        System.out.println(lastNumber);
     }
 }
