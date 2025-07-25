8ebcc7a03544bfbf1cd04024ada8262df82d64e6
ChuckleScriptCraft <hxzzmxnch@gmail.com>
2025-07-26 00:22:51 +0100

    Day 19 2020 - Part Two

--- 2020/19/day_19.java
+++ 2020/19/day_19.java
@@ -4,13 +4,30 @@ import java.io.IOException;
 import java.util.*;
 
 public class day_19 {
-    private static boolean allAB(ArrayList<String> st) {
-        for (String s : st) {
-            if (!s.matches("a|b")) {
-                return false;
+
+    private static List<Integer> match(Map<String, ArrayList<ArrayList<String>>> rules, String ruleNum, String message,
+            int index) {
+        ArrayList<ArrayList<String>> options = rules.get(ruleNum);
+        List<Integer> result = new ArrayList<>();
+        for (ArrayList<String> option : options) {
+            List<Integer> idxs = new ArrayList<>();
+            idxs.add(index);
+            for (String token : option) {
+                List<Integer> newIdxs = new ArrayList<>();
+                for (int idx : idxs) {
+                    if (token.equals("a") || token.equals("b")) {
+                        if (idx < message.length() && message.charAt(idx) == token.charAt(0)) {
+                            newIdxs.add(idx + 1);
+                        }
+                    } else {
+                        newIdxs.addAll(match(rules, token, message, idx));
+                    }
+                }
+                idxs = newIdxs;
             }
+            result.addAll(idxs);
         }
-        return true;
+        return result;
     }
 
     public static void main(String[] args) throws IOException {
@@ -21,7 +38,6 @@ public class day_19 {
 
         // populate the dimension with the starting state
         while ((line = reader.readLine()) != null) {
-            // System.out.println(line);
             if (line.contains(":")) {
                 String key = line.substring(0, line.indexOf(":"));
                 String value = line.substring(line.indexOf(":") + 2).replace("\"", "");
@@ -39,47 +55,29 @@ public class day_19 {
             }
         }
         reader.close();
-        // Start with number 0 and build up a stack
-        ArrayList<ArrayList<String>> stack = new ArrayList<>();
-        stack.add(map.get("0").get(0));
-        HashSet<String> dutty = new HashSet<>();
 
-        while (!stack.isEmpty()) {
-            ArrayList<String> top = stack.remove(stack.size() - 1);
-            if (allAB(top)) {
-                dutty.add(String.join("", top));
-                continue;
-            }
+        // PART 2 MODIFICATION: update rules 8 and 11
+        if (map.containsKey("8")) {
+            ArrayList<ArrayList<String>> rule8 = new ArrayList<>();
+            rule8.add(new ArrayList<>(List.of("42")));
+            rule8.add(new ArrayList<>(List.of("42", "8")));
+            map.put("8", rule8);
+        }
+        if (map.containsKey("11")) {
+            ArrayList<ArrayList<String>> rule11 = new ArrayList<>();
+            rule11.add(new ArrayList<>(List.of("42", "31")));
+            rule11.add(new ArrayList<>(List.of("42", "11", "31")));
+            map.put("11", rule11);
+        }
 
-            ArrayList<ArrayList<String>> a = new ArrayList<>();
-            a.add(new ArrayList<>());
-            for (String i : top) {
-
-                ArrayList<ArrayList<String>> temp = new ArrayList<>();
-                if (i.matches("\\d+")) {
-                    temp = map.get(i);
-                } else {
-                    temp.add(new ArrayList<>(List.of(i)));
-                }
-                ArrayList<ArrayList<String>> b = new ArrayList<>();
-                for (ArrayList<String> x : a) {
-                    for (ArrayList<String> y : temp) {
-                        ArrayList<String> z = new ArrayList<>(x);
-                        z.addAll(y);
-                        b.add(z);
-                    }
-                }
-                a = b;
-            }
-            // Add back onto the stack
-            for (ArrayList<String> x : a) {
-                stack.add(x);
+        int count = 0;
+        for (String msg : values) {
+            List<Integer> res = match(map, "0", msg, 0);
+            if (res.contains(msg.length())) {
+                count++;
             }
         }
-        // Part One
-        HashSet<String> intersection = new HashSet<>(dutty);
-        intersection.retainAll(values);
-        System.out.println(intersection.size());
+        System.out.println(count);
     }
 
 }
