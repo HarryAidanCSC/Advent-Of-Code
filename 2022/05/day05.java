4ba99196891e71daa81fbace1633bab9a44ae007
Harry <155183070+HarryAidanCSC@users.noreply.github.com>
2025-08-22 16:29:55 +0000

    Day 05 2022 - Part Two

--- 2022/05/day05.java
+++ 2022/05/day05.java
@@ -9,11 +9,12 @@ import java.util.regex.*;
 
 public class day05 {
     public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new FileReader("test.txt"));
+        BufferedReader reader = new BufferedReader(new FileReader("input.txt"));
         String line;
         Pattern pattern = Pattern.compile("\\d+");
 
         Map<Integer, Deque<Character>> stacks = new HashMap<>();
+        Map<Integer, Deque<Character>> jacks = new HashMap<>();
 
         // Enter logic
         while ((line = reader.readLine()) != null) {
@@ -23,6 +24,7 @@ public class day05 {
                     if (line.charAt(i) != ' ') {
                         int idx = (1 + (i / 4));
                         stacks.computeIfAbsent(idx, k -> new ArrayDeque<>()).push(line.charAt(i));
+                        jacks.computeIfAbsent(idx, k -> new ArrayDeque<>()).push(line.charAt(i));
                     }
                 }
             }
@@ -33,16 +35,24 @@ public class day05 {
                 int fromStack = -1;
                 while (matcher.find()) {
                     if (numValues < 0) numValues = Integer.parseInt(matcher.group());
-                    else if (fromStack < 0) fromStack = Integer.parseInt(matcher.group());
-                    else {
+                    else if (fromStack < 0) {
+                        fromStack = Integer.parseInt(matcher.group());
+                    } else {
+                        char[] valuesToMove = new char[numValues];
                         int toStack = Integer.parseInt(matcher.group());
                         for (int i = 0; i < numValues; i++){
                             stacks.get(toStack).add(stacks.get(fromStack).pollLast());
+                            valuesToMove[i] = jacks.get(fromStack).pollLast();
+                        }
+                        for (int i = numValues - 1; i >= 0; i--) {
+                            jacks.get(toStack).add(valuesToMove[i]);
                         }
                     }
                 }
             }            
         }
         for (int i = 1; i <= stacks.size(); i++) System.out.print(stacks.get(i).peekLast());
+        System.out.print("\n");
+        for (int i = 1; i <= jacks.size(); i++) System.out.print(jacks.get(i).peekLast());
     }
 }
