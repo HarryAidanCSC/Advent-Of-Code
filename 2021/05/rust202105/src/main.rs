5eed20b5ff9dfdc8d97bddac65debdef4e6f4dbd
ChuckleScriptCraft <hxzzmxnch@gmail.com>
2025-11-24 20:54:40 +0000

    Day 05 2021 - Part Two

--- 2021/05/rust202105/src/main.rs
+++ 2021/05/rust202105/src/main.rs
@@ -7,7 +7,10 @@ fn main() {
     let lines: Vec<&str> = file.lines().collect(); 
 
     let mut coords: HashMap<(i32, i32), i32> = HashMap::new();
+    let mut coordsp2: HashMap<(i32, i32), i32> = HashMap::new();
+
     let mut p1 = 0;
+    let mut p2 = 0;
     for line in &lines{
         let split_line: Vec<i32> = line
             .split(" -> ")
@@ -15,24 +18,45 @@ fn main() {
             .map(|s| s.parse().unwrap())
             .collect();
 
+        let minx = min(split_line[0], split_line[2]);
+        let maxx = max(split_line[0], split_line[2]);
+        let miny = min(split_line[1], split_line[3]);
+        let maxy = max(split_line[1], split_line[3]);
 
-        if (split_line[0] != split_line[2]) && (split_line[1] != split_line[3]){
-            continue;
+        if (minx != maxx) && (miny != maxy){
+            let mut i = 0;
+            let (x, y, x1) = (split_line[0], split_line[1], split_line[2]);
+            let x_dir = if x == minx { 1 } else { -1 };
+            let y_dir = if y == miny { 1 } else { -1 };
+            while x + ((i-1)*x_dir) != x1 {
+                let count = coordsp2.entry((x + (i * x_dir), y + (i * y_dir))).or_insert(0);
+                *count += 1;
+                if *count == 2 {
+                    p2 += 1;
+                }
+                i += 1;
+            }
         } else {
-            println!("{:?}", split_line);
-        }
         // Iterate through the line
-        for x in min(split_line[0], split_line[2])..=max(split_line[0], split_line[2]){
-            for y in min(split_line[1], split_line[3])..=max(split_line[1], split_line[3]){
+        for x in minx..=maxx{
+            for y in miny..=maxy{
+                // P1
                 let count = coords.entry((x,y)).or_insert(0);
                 *count += 1;
                 if *count == 2 {
                     p1 += 1;
                 }
+                // P2
+                let count = coordsp2.entry((x,y)).or_insert(0);
+                *count += 1;
+                if *count == 2 {
+                    p2 += 1;
+                }
+            }
             }
         }
         
     }
-    println!("{:?}", coords);
     println!("Part One: {}", p1);
+    println!("Part Two: {}", p2);
 }
