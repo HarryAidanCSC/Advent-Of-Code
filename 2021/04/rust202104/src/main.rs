6fc1a7b4db5ad2919095dde39ddc5e0568f331e2
ChuckleScriptCraft <hxzzmxnch@gmail.com>
2025-11-24 15:54:26 +0000

    Day 04 2021 - Part Two

--- 2021/04/rust202104/src/main.rs
+++ 2021/04/rust202104/src/main.rs
@@ -4,8 +4,7 @@ use std::collections::HashSet;
 
 fn main() {
     let file = fs::read_to_string("../../input.txt").unwrap();
-    let lines: Vec<&str> = file.lines().collect();  
-    println!("{:?}", &lines[2..]);
+    let lines: Vec<&str> = file.lines().collect(); 
     
     // Parse input
     let header: Vec<&str> = lines[0].split(',').collect();
@@ -15,7 +14,7 @@ fn main() {
     let mut bingo_scores: HashMap<i32, Vec<i32>> = HashMap::new();
     let mut bingo_poss: HashMap<i32, MyBingoPos> = HashMap::new();
     let mut bingo_unseen: HashMap<i32, HashSet<String>> = HashMap::new();
-    let mut inital_bingo_scores = vec![5; 10];
+    let inital_bingo_scores = vec![5; 10];
 
     let mut i = 0;
     let mut n = 0;
@@ -49,10 +48,12 @@ fn main() {
 
 
     // Iterate through the header
-    let mut p1 = 0;
-    let mut breakflag = false;
+    let mut winners_round: Vec<i32>= vec![];
+    let mut winners_score: HashMap<i32, i32> = HashMap::new();
+
     for num in header.iter(){
-        for (key, value) in &bingo_poss{
+        for key in bingo_poss.keys(){
+            if winners_score.contains_key(key){ continue; }
             // 1. Remove from unseen values
             bingo_unseen.get_mut(key).unwrap().remove(*num);
             // 2. Update scores
@@ -62,22 +63,25 @@ fn main() {
                 
                 // Win condition
                 if (bingo_scores.get(&key).unwrap()[x as usize] == 1) || (bingo_scores.get(&key).unwrap()[y as usize] == 1) {
+                    let mut cur_score = 0;
+
                     for value in bingo_unseen.get(key).unwrap() {
-                        p1 += value.parse::<i32>().unwrap();
+                        cur_score += value.parse::<i32>().unwrap();
                     } 
-                    p1 *= num.parse::<i32>().unwrap();
-                    breakflag = true;
-                    break;
+                    cur_score *= num.parse::<i32>().unwrap();
+
+                    winners_round.push(*key);
+                    winners_score.insert(*key, cur_score);
+                    continue;;
                 }
 
                 // // -1 from score of corresponding row/col
                 bingo_scores.get_mut(&key).unwrap()[x as usize] -= 1;
                 bingo_scores.get_mut(&key).unwrap()[y as usize] -= 1;
-                println!("{:?}", bingo_scores.get(&key).unwrap()[0]);
                 
             }
         }
-        if breakflag { break; }
     }
-    println!("Part One: {}", p1);
+    println!("Part One: {:?}", winners_score.get(&winners_round[0]).unwrap());
+    println!("Part Two: {:?}", winners_score.get(&winners_round[winners_round.len() -1]).unwrap());
 }
