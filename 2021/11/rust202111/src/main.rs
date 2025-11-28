c4ca548ed88f4228d0f6cf636e84416bfa5587f2
ChuckleScriptCraft <hxzzmxnch@gmail.com>
2025-11-28 23:45:26 +0000

    Day 11 2021 - Part One

---
+++ 2021/11/rust202111/src/main.rs
@@ -0,0 +1,57 @@
+use std::collections::HashSet;
+use std::collections::VecDeque;
+use std::fs;
+
+fn main() {
+    let file = fs::read_to_string("../../input.txt").unwrap();
+    let lines: Vec<Vec<i32>> = file
+        .lines()
+        .map(|l| l.chars().map(|c| c.to_digit(10).unwrap() as i32).collect())
+        .collect();
+
+    let mut p1 = 0;
+    let mut mlines = lines.clone();
+    for _ in 0..100 {
+        let mut queue: VecDeque<(usize, usize)> = VecDeque::new();
+
+        // Add energy to every octopus and keep track of flashing ones
+        for (i, row) in mlines.iter_mut().enumerate() {
+            for (j, value) in row.iter_mut().enumerate() {
+                *value += 1;
+                *value %= 10;
+                if value == &0 {
+                    queue.push_front((i, j));
+                }
+            }
+        }
+
+        // Flash the octopi
+        while let Some((cur_i, cur_j)) = queue.pop_back() {
+            p1 += 1;
+
+            // Update neighbours
+            for di in -1..=1 {
+                for dj in -1..=1 {
+                    if !(di == 0 && dj == 0) {
+                        let (ni, nj) = ((cur_i as i32 + di) as usize, (cur_j as i32 + dj) as usize);
+
+                        if let Some(neighbour) =
+                            mlines.get_mut(ni as usize).and_then(|r| r.get_mut(nj))
+                        {
+                            if neighbour == &0 {
+                                continue;
+                            }
+                            *neighbour += 1;
+                            *neighbour %= 10;
+                            if neighbour == &0 {
+                                queue.push_front((ni, nj));
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    println!("Part One: {}", p1);
+}
