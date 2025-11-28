e09c6c8c1fd00772455a078be107a8eaa73db252
ChuckleScriptCraft <hxzzmxnch@gmail.com>
2025-11-28 23:59:26 +0000

    Day 11 2021 - Part Two

--- 2021/11/rust202111/src/main.rs
+++ 2021/11/rust202111/src/main.rs
@@ -1,4 +1,3 @@
-use std::collections::HashSet;
 use std::collections::VecDeque;
 use std::fs;
 
@@ -11,8 +10,10 @@ fn main() {
 
     let mut p1 = 0;
     let mut mlines = lines.clone();
-    for _ in 0..100 {
+    let octopi = (lines.len() as i32) * (lines[0].len() as i32);
+    for i in 0..2500 {
         let mut queue: VecDeque<(usize, usize)> = VecDeque::new();
+        let mut octopussy = 0;
 
         // Add energy to every octopus and keep track of flashing ones
         for (i, row) in mlines.iter_mut().enumerate() {
@@ -27,7 +28,7 @@ fn main() {
 
         // Flash the octopi
         while let Some((cur_i, cur_j)) = queue.pop_back() {
-            p1 += 1;
+            octopussy += 1;
 
             // Update neighbours
             for di in -1..=1 {
@@ -51,7 +52,14 @@ fn main() {
                 }
             }
         }
-    }
 
-    println!("Part One: {}", p1);
+        p1 += octopussy;
+        if i == 99 {
+            println!("Part One: {}", p1);
+        }
+        if octopussy == octopi {
+            println!("Part Two {}", i + 1);
+            break;
+        }
+    }
 }
