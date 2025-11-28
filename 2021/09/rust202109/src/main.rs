43c6dfd2bf15d48e396530a8ce0a46578f4b2390
ChuckleScriptCraft <hxzzmxnch@gmail.com>
2025-11-28 21:52:35 +0000

    Day 09 2021 - Part Two

--- 2021/09/rust202109/src/main.rs
+++ 2021/09/rust202109/src/main.rs
@@ -1,4 +1,6 @@
+use std::collections::HashSet;
 use std::fs;
+use std::hash::Hash;
 
 fn main() {
     let file = fs::read_to_string("../../input.txt").unwrap();
@@ -11,10 +13,16 @@ fn main() {
     let mut p1 = 0;
     let directions = [(0, 1), (1, 0), (-1, 0), (0, -1)];
 
+    let mut nines_shining: HashSet<(usize, usize)> = HashSet::new();
+
     for (i, row) in lines.iter().enumerate() {
         for (j, value) in row.iter().enumerate() {
             let mut skip = false;
 
+            // p2 logic
+            if value == &9 {
+                nines_shining.insert((i, j));
+            }
             for (di, dj) in directions.iter() {
                 let (ni, nj) = (i as i32 + di, j as i32 + dj);
                 if let Some(neighbour) = lines.get(ni as usize).and_then(|r| r.get(nj as usize)) {
@@ -30,5 +38,44 @@ fn main() {
         }
     }
 
+    // P2
+    let mut basins: Vec<i64> = vec![];
+    let mut universe_seen = nines_shining.clone();
+    for (i, row) in lines.iter().enumerate() {
+        for j in 0..row.len() {
+            // Skip already seen
+            if universe_seen.contains(&(i, j)) {
+                continue;
+            }
+            // Create BFS
+            let mut stack = vec![(i, j)];
+            let mut cur_basin = 0;
+            while let Some((cur_i, cur_j)) = stack.pop() {
+                if universe_seen.contains(&(cur_i, cur_j)) {
+                    continue;
+                } else {
+                    universe_seen.insert((cur_i, cur_j));
+                    cur_basin += 1;
+                }
+                // Find value
+                for (di, dj) in directions.iter() {
+                    let (ni, nj) = (cur_i as i32 + di, cur_j as i32 + dj);
+
+                    if ni >= 0 && nj >= 0 && ni < lines.len() as i32 && nj < lines[0].len() as i32 {
+                        let coords = (ni as usize, nj as usize);
+                        if !universe_seen.contains(&coords) {
+                            stack.insert(0, coords);
+                        }
+                    }
+                }
+            }
+            basins.push(cur_basin);
+        }
+    }
+
+    basins.sort_by(|a, b| b.cmp(a));
+    let p2: i64 = basins.iter().take(3).product();
+
     println!("Part One: {}", p1);
+    println!("Part Two: {}", p2);
 }
