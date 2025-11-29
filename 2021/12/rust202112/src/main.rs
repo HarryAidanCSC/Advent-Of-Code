118b34c2f3b091f14d29ed2fc127fc01b7fa7f6b
ChuckleScriptCraft <hxzzmxnch@gmail.com>
2025-11-29 12:36:50 +0000

    Day 12 2021 - Part Two

--- 2021/12/rust202112/src/main.rs
+++ 2021/12/rust202112/src/main.rs
@@ -18,7 +18,7 @@ fn main() {
         nodes.entry(a.to_string()).or_default().push(b.to_string());
         nodes.entry(b.to_string()).or_default().push(a.to_string());
     }
-    // cur_node, path
+    // P1
     let mut p1 = 0;
     while let Some((cur_node, path)) = queue.pop_front() {
         for nb in nodes.get(&cur_node).unwrap().iter() {
@@ -34,5 +34,33 @@ fn main() {
         }
     }
 
+    // P2
+    let mut p2 = 0;
+    let mut queue: VecDeque<(String, HashSet<String>, bool)> =
+        VecDeque::from([(String::from("start"), HashSet::new(), true)]);
+    while let Some((cur_node, path, cond)) = queue.pop_front() {
+        for nb in nodes.get(&cur_node).unwrap().iter() {
+            if nb == "end" {
+                p2 += 1;
+            } else if nb != "start" {
+                let mut new_path = path.clone();
+                let mut new_cond = cond;
+
+                if !nb.chars().any(|c| c.is_uppercase()) {
+                    if !path.contains(nb) {
+                        new_path.insert(nb.to_string());
+                    } else if path.contains(nb) && new_cond {
+                        new_cond = false;
+                    } else {
+                        continue;
+                    }
+                }
+
+                queue.push_back((nb.to_string(), new_path, new_cond));
+            }
+        }
+    }
+
     println!("Part One: {}", p1);
+    println!("Part Two: {}", p2);
 }
