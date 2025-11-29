64b4fa7c2a4ef05b2149f21da16148de5c629220
ChuckleScriptCraft <hxzzmxnch@gmail.com>
2025-11-29 13:22:04 +0000

    Day 13 2021 - Part Two

---
+++ 2021/13/rust202113/src/main.rs
@@ -0,0 +1,43 @@
+use std::collections::HashSet;
+use std::fs;
+
+fn main() {
+    let file = fs::read_to_string("../../input.txt").unwrap();
+    let lines: Vec<&str> = file.lines().collect();
+
+    let mut coords = HashSet::new();
+    let mut p1 = 0;
+    for line in lines.iter() {
+        if line.is_empty() {
+            continue;
+        } else if line.starts_with('f') {
+            let (_, axis) = line.split_once("=").unwrap();
+            let axis = axis.parse::<i32>().unwrap();
+            let mut new_coords = coords.clone();
+
+            if line.contains('x') {
+                for (x, y) in coords {
+                    if x > axis {
+                        new_coords.remove(&(x, y));
+                        new_coords.insert((2 * axis - x, y));
+                    }
+                }
+            } else if line.contains('y') {
+                for (x, y) in coords {
+                    if y > axis {
+                        new_coords.remove(&(x, y));
+                        new_coords.insert((x, 2 * axis - y));
+                    }
+                }
+            }
+            coords = new_coords.clone();
+            if p1 == 0 {
+                p1 = new_coords.len() as i32;
+            }
+        } else {
+            let (x, y) = line.split_once(",").unwrap();
+            coords.insert((x.parse::<i32>().unwrap(), y.parse::<i32>().unwrap()));
+        }
+    }
+    println!("Part One: {}", p1);
+}
