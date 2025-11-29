722720f2f50b5abc393b6837bf5e47e03ac25115
ChuckleScriptCraft <hxzzmxnch@gmail.com>
2025-11-29 12:14:08 +0000

    Day 12 2021 - Part One

---
+++ 2021/12/rust202112/src/main.rs
@@ -0,0 +1,38 @@
+use std::collections::HashMap;
+use std::collections::HashSet;
+use std::collections::VecDeque;
+use std::fs;
+
+fn main() {
+    let file = fs::read_to_string("../../input.txt").unwrap();
+    let lines: Vec<&str> = file.lines().collect();
+
+    let mut nodes: HashMap<String, Vec<String>> = HashMap::new();
+    let mut queue: VecDeque<(String, HashSet<String>)> = VecDeque::from([(
+        String::from("start"),
+        HashSet::from([String::from("start")]),
+    )]);
+
+    for line in lines.iter() {
+        let (a, b) = line.split_once("-").unwrap();
+        nodes.entry(a.to_string()).or_default().push(b.to_string());
+        nodes.entry(b.to_string()).or_default().push(a.to_string());
+    }
+    // cur_node, path
+    let mut p1 = 0;
+    while let Some((cur_node, path)) = queue.pop_front() {
+        for nb in nodes.get(&cur_node).unwrap().iter() {
+            if nb == "end" {
+                p1 += 1;
+            } else if !path.contains(nb) {
+                let mut new_path = path.clone();
+                if !nb.chars().any(|c| c.is_uppercase()) {
+                    new_path.insert(nb.to_string());
+                }
+                queue.push_back((nb.to_string(), new_path));
+            }
+        }
+    }
+
+    println!("Part One: {}", p1);
+}
