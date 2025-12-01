a39d17ecf6d9e045a0d647922542a4f449b179ea
ChuckleScriptCraft <hxzzmxnch@gmail.com>
2025-12-01 14:54:45 +0000

    Day 01 2025 - Part One

--- 2025/01/rust_202501/src/main.rs
+++ 2025/01/rust_202501/src/main.rs
@@ -3,28 +3,32 @@ use std::fs;
 fn main() {
     let file = fs::read_to_string("../../input.txt").unwrap();
     let lines: Vec<&str> = file.lines().collect();
-    let mut p1 = 50;
-    let mut p1_score = 0;
+    let mut p1 = 0;
+    let mut p2 = 0;
+    let mut cur_score = 50;
 
     for line in lines {
-        let mut num = line[1..].parse::<i32>().unwrap() % 100;
-        if line.contains('L') {
-            num *= -1;
-        }
-        p1 += num;
+        let numero = line[1..].parse::<i32>().unwrap();
+        let num = if line.contains('L') { -1 } else { 1 };
 
-        if p1 < 0 {
-            p1 -= 100;
-            p1 %= 100;
-            p1 += 100;
-        } else if p1 >= 100 {
-            p1 %= 100;
+        for _ in 0..numero {
+            cur_score += num;
+            if cur_score == -1 {
+                cur_score = 99;
+            } else if cur_score == 100 {
+                cur_score = 0;
+            }
+
+            if cur_score == 0 {
+                p2 += 1;
+            }
         }
 
-        if p1 == 0 {
-            p1_score += 1;
+        if cur_score == 0 {
+            p1 += 1;
         }
     }
 
-    println!("Part One : {}", p1_score);
+    println!("Part One : {}", p1);
+    println!("Part Two : {}", p2);
 }
