f7d9ed35e35d0cf2490c0bf3732c74795366e765
ChuckleScriptCraft <hxzzmxnch@gmail.com>
2025-12-02 09:49:55 +0000

    Day 02 2025 - Part Two

--- 2025/02/rust_202502/src/main.rs
+++ 2025/02/rust_202502/src/main.rs
@@ -1,36 +1,49 @@
+use std::collections::HashSet;
 use std::fs;
 
 fn main() {
     let file = fs::read_to_string("../../input.txt").unwrap();
     let lines: Vec<&str> = file.lines().collect::<Vec<&str>>()[0].split(',').collect();
-    println!("{:?}", lines);
 
     let mut p1: i64 = 0;
+    let mut p2: i64 = 0;
     for line in lines {
         let (start, end) = line.split_once('-').unwrap();
         let start: i64 = start.parse().unwrap();
         let end: i64 = end.parse().unwrap();
-
+        let mut invalid_ids = HashSet::new();
         for n in start..=end {
-            let n_str: Vec<char> = n.to_string().chars().collect();
-            let mut cond = true;
-            let (mut left, right) = (0 as usize, n_str.len());
-            if right % 2 == 1 {
-                continue;
-            }
-            let mut mid = right / 2;
-            while mid < right {
-                if n_str[left] != n_str[mid] {
-                    cond = false;
-                    break;
+            let n_str: String = n.to_string();
+            let right = n_str.len();
+            let mid = right / 2;
+
+            for i in 1..=mid {
+                if right % i != 0 {
+                    continue;
                 }
-                left += 1;
-                mid += 1;
+
+                let mut cond = true;
+                let ref_str = &n_str[0..i];
+                for z in (i..right).step_by(i) {
+                    let ns = &n_str[z..z + i];
+                    if ns != ref_str {
+                        cond = false;
+                        break;
+                    }
+                }
+
+                if cond {
+                    invalid_ids.insert(n);
+                    if i == mid && right % 2 == 0 {
+                        p1 += n;
+                    }
+                }
             }
-            if cond {
-                p1 += n;
-            }
+        }
+        for id in invalid_ids {
+            p2 += id;
         }
     }
     println!("Part One: {}", p1);
+    println!("Part One: {}", p2);
 }
