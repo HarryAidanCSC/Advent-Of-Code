d95086f707226734d78e639296707c833af31071
ChuckleScriptCraft <hxzzmxnch@gmail.com>
2025-12-03 10:10:51 +0000

    Day 03 2025 - Part Two

--- 2025/03/rust_202503/src/main.rs
+++ 2025/03/rust_202503/src/main.rs
@@ -23,6 +23,41 @@ fn main() {
         }
         p1 += cur_max;
     }
-    // println!("{:?}", lines);
+
+    let mut p2: i64 = 0;
+    for mut line in lines {
+        let mut to_remove = line.len() - 12;
+        let mut i = 0;
+
+        while to_remove > 0 && i < line.len() {
+            let end_limit = (i + to_remove + 1).min(line.len());
+
+            let max_val = line[i..end_limit].iter().copied().max().unwrap_or(0);
+
+            let relative_idx = line[i..end_limit]
+                .iter()
+                .position(|&x| x == max_val)
+                .unwrap();
+            let absolute_idx = i + relative_idx;
+
+            for k in i..absolute_idx {
+                line[k] = 0;
+                to_remove -= 1;
+            }
+
+            i = absolute_idx + 1;
+        }
+
+        let mut cur: i64 = 0;
+        let mut f: i64 = 1;
+        line.retain(|&x| x != 0);
+        for val in line[..12].iter().rev() {
+            cur += f * *val as i64;
+            f *= 10;
+        }
+        println!("{:?}", cur);
+        p2 += cur;
+    }
     println!("Part One: {}", p1);
+    println!("Part Two: {}", p2);
 }
