c202431fbcc667abc3c44c8443531389f799536a
ChuckleScriptCraft <hxzzmxnch@gmail.com>
2025-12-05 16:08:01 +0000

    Day  07 2022 - Part One

---
+++ 2022/07/rust_202207/src/main.rs
@@ -0,0 +1,61 @@
+use std::{collections::HashMap, fs};
+
+fn dfs<'a>(
+    node: &str,
+    children: &HashMap<String, Vec<String>>,
+    sizes: &HashMap<String, i64>,
+) -> (i64, i64) {
+    let mut cur_size = *sizes.get(node).unwrap_or(&0);
+    let mut asu = 0;
+
+    // Go through children
+    if let Some(child_nodes) = children.get(node) {
+        for child in child_nodes {
+            let (child_score, p1p) = dfs(child, children, sizes);
+            asu += p1p;
+            cur_size += child_score;
+        }
+    }
+
+    //
+    if cur_size <= 100000 {
+        asu += cur_size;
+    }
+    return (cur_size, asu);
+}
+
+fn main() {
+    let file = fs::read_to_string("../../input.txt").unwrap();
+    let lines = file.lines();
+
+    let mut stack: Vec<&str> = vec![];
+    let mut dir_size: HashMap<String, i64> = HashMap::new();
+    let mut children: HashMap<String, Vec<String>> = HashMap::new();
+
+    for line in lines.skip(1) {
+        if line.starts_with("$ cd") {
+            let loc = &line[5..];
+            if loc == ".." {
+                stack.pop();
+            } else {
+                stack.push(loc);
+            }
+        } else if line.starts_with("$ ls") {
+            continue;
+        } else {
+            let cur_dir: String = format!("/{}", stack.join("/"));
+            if line.starts_with("dir") {
+                let child: String = format!("{}/{}", cur_dir, &line[4..]).replace("//", "/");
+                children.entry(cur_dir).or_default().push(child);
+            } else {
+                let (size, _file) = line.split_once(' ').unwrap();
+                let size = size.parse::<i64>().unwrap();
+                *dir_size.entry(cur_dir).or_default() += size;
+            }
+        }
+    }
+
+    // DFS
+    let (_, p1) = dfs("/", &children, &dir_size);
+    println!("Part One {}", p1);
+}
