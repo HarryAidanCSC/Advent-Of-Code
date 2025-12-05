bf5eacc9faa371b50c394d3034877faeb8db95a4
ChuckleScriptCraft <hxzzmxnch@gmail.com>
2025-12-05 22:00:33 +0000

    Day 09 2022 - Part One

--- 2022/09/rust_202309/src/main.rs
+++ 2022/09/rust_202309/src/main.rs
@@ -8,25 +8,66 @@ fn main() {
         .map(|s| (s.0.chars().next().unwrap(), s.1.parse::<i32>().unwrap()))
         .collect();
 
-    let mut visited = HashSet::from([(0, 0)]);
-    let (mut h, mut t) = ((0, 0), (0, 0));
+    // P1
+    let mut visited0 = HashSet::from([(0, 0)]);
+    let mut visited1 = HashSet::from([(0, 0)]);
+    let mut h = (0, 0);
+    let mut ts = [(0, 0); 9];
 
     for (d, n) in lines {
         for _ in 0..n {
-            let prev_h = h.clone();
             match d {
-                'U' => h.0 += 1,
-                'D' => h.0 -= 1,
-                'R' => h.1 += 1,
-                'L' => h.1 -= 1,
+                'U' => h.1 += 1,
+                'D' => h.1 -= 1,
+                'R' => h.0 += 1,
+                'L' => h.0 -= 1,
                 _ => print!(""),
             }
-            if i32::abs(t.0 - h.0) > 1 || i32::abs(t.1 - h.1) > 1 {
-                visited.insert(prev_h);
-                t = prev_h;
+
+            let mut nts = [(0, 0); 9];
+            let mut p = h;
+            for (i, t) in ts.iter().enumerate() {
+                let (xdiff, ydiff): (i32, i32) = (p.0 - t.0, p.1 - t.1);
+
+                // X
+                let mut dx = 0;
+                if xdiff < 0 {
+                    dx = -1;
+                } else if xdiff > 0 {
+                    dx = 1;
+                }
+
+                // Y
+                let mut dy = 0;
+                if ydiff < 0 {
+                    dy = -1;
+                } else if ydiff > 0 {
+                    dy = 1;
+                }
+
+                let mut newt = *t;
+                if xdiff.abs() + ydiff.abs() >= 3 {
+                    newt = (newt.0 + dx, newt.1 + dy);
+                } else if xdiff.abs() == 2 && ydiff.abs() == 0 {
+                    newt = (newt.0 + dx, newt.1);
+                } else if xdiff.abs() == 0 && ydiff.abs() == 2 {
+                    newt = (newt.0, newt.1 + dy);
+                } else {
+                    newt = (newt.0, newt.1);
+                }
+
+                nts[i] = newt;
+                p = newt;
             }
+
+            ts = nts;
+            visited0.insert(ts[0]);
+            visited1.insert(ts[8]);
         }
     }
 
-    println!("Part One: {}", visited.len());
+    // P2
+
+    println!("Part One: {}", visited0.len());
+    println!("Part Two: {}", visited1.len());
 }
