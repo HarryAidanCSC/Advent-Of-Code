2fbacfa7b79624805fb8610191df2a701ffca39c
ChuckleScriptCraft <hxzzmxnch@gmail.com>
2025-12-05 14:17:32 +0000

    Day 06 2022 - Part Two

--- 2022/06/rust_202006/src/main.rs
+++ 2022/06/rust_202006/src/main.rs
@@ -4,17 +4,19 @@ fn main() {
     let file = fs::read_to_string("../../input.txt").unwrap();
     let lines: Vec<char> = file.lines().next().unwrap().chars().collect();
 
-    let mut cur = lines[..4].to_vec();
-    let mut p1 = 0;
+    let mut parts = vec![];
+    for j in [4, 14] {
+        let mut cur = lines[..j].to_vec();
 
-    for i in 4..lines.len() {
-        cur.remove(0);
-        cur.push(lines[i]);
-        if cur.len() == cur.iter().collect::<HashSet<_>>().len() {
-            p1 = i + 1;
-            break;
+        for i in j..lines.len() {
+            cur.remove(0);
+            cur.push(lines[i]);
+            if cur.len() == cur.iter().collect::<HashSet<_>>().len() {
+                parts.push(i + 1);
+                break;
+            }
         }
     }
-
-    println!("Part One: {}", p1);
+    println!("Part One: {}", parts[0]);
+    println!("Part One: {}", parts[1]);
 }
