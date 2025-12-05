be9165a3afdc5a51da6857b93d2219f777b16126
ChuckleScriptCraft <hxzzmxnch@gmail.com>
2025-12-05 22:56:15 +0000

    Day 10 2022 - Part Two

--- 2022/10/rust_202210/src/main.rs
+++ 2022/10/rust_202210/src/main.rs
@@ -4,27 +4,38 @@ fn main() {
     let lines: Vec<&str> = file.lines().collect();
 
     let mut p1 = 0;
-    let mut cycle = 0;
-    let mut cur = 1;
+    let mut cycle: i32 = 0;
+    let mut cur: i32 = 1;
+    let mut render: [[char; 40]; 6] = [['.'; 40]; 6];
 
     for line in lines {
+        // P2
+        let pos = cycle % 40;
+        println!("Pos {}  Reg {}", pos, cur);
+        if cur >= pos - 1 && cur < (pos + 2) {
+            render[(cycle / 40) as usize][pos as usize] = '#';
+        }
+
         if line.starts_with("noop") {
             cycle += 1;
             if (cycle + 20) % 40 == 0 {
-                println!("1. {} Cycle: {} Cur {}", cycle * cur, cycle, cur);
                 p1 += cycle * cur;
             }
-            // Add
         } else {
             cycle += 1;
             if (cycle + 20) % 40 == 0 {
-                println!("2. {} Cycle: {} Cur {}", cycle * cur, cycle, cur);
                 p1 += cycle * cur;
             }
 
+            // P2
+            let pos = cycle % 40;
+
+            if cur >= pos - 1 && cur < (pos + 2) {
+                render[(cycle / 40) as usize][pos as usize] = '#';
+            }
+            //
             cycle += 1;
             if (cycle + 20) % 40 == 0 {
-                println!("3. {} Cycle: {} Cur {}", cycle * cur, cycle, cur);
                 p1 += cycle * cur;
             }
 
@@ -34,4 +45,8 @@ fn main() {
         }
     }
     println!("Part One: {}", p1);
+    for line in render {
+        let render_line: String = line.iter().collect();
+        println!("{}", render_line)
+    }
 }
