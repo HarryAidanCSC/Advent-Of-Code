bd48d04bfe97be1b51226c404532162aaf64ae3b
ChuckleScriptCraft <hxzzmxnch@gmail.com>
2025-12-08 15:56:23 +0000

    Day 12 2022 - Part Two

--- 2022/12/rust_202212/src/main.rs
+++ 2022/12/rust_202212/src/main.rs
@@ -9,16 +9,22 @@ fn main() {
         .collect();
 
     let (mut start, mut end) = ((0, 0, 97 as u8, 0 as usize), (0, 0));
+    let mut p2_start = VecDeque::new();
+    let mut best2 = HashMap::new();
     for (j, line) in lines.iter().enumerate() {
         for (i, val) in line.iter().enumerate() {
             if val == &69 {
                 end = (j, i);
             } else if val == &83 {
                 start = (j, i, 97, 0);
+            } else if val == &97 {
+                p2_start.push_front((j, i, 97, 0));
+                best2.insert((j, i), 0);
             }
         }
     }
 
+    lines[start.0 as usize][start.1 as usize] = 97;
     lines[end.0 as usize][end.1 as usize] = 122;
 
     let mut queue = VecDeque::from([start.clone()]);
@@ -30,7 +36,6 @@ fn main() {
         if best.get(&(y, x)).unwrap() < &visit_n {
             continue;
         }
-
         // Neighbours
         for (dy, dx) in DELTAS {
             let (ny, nx) = (y as i32 + dy, x as i32 + dx);
@@ -54,5 +59,34 @@ fn main() {
         }
     }
 
+    // P2
+    let mut p2 = 0;
+    'pork: while let Some((y, x, cur, visit_n)) = p2_start.pop_front() {
+        if best.get(&(y, x)).unwrap() < &visit_n {
+            continue;
+        }
+        // Neighbours
+        for (dy, dx) in DELTAS {
+            let (ny, nx) = (y as i32 + dy, x as i32 + dx);
+
+            if let Some(&v) = lines.get(ny as usize).and_then(|row| row.get(nx as usize)) {
+                let (ny, nx) = (ny as usize, nx as usize);
+                if best2.contains_key(&(ny, nx)) && *best2.get(&(ny, nx)).unwrap() <= visit_n + 1 {
+                    continue;
+                } else {
+                    if v <= cur + 1 {
+                        // END condition
+                        if (ny, nx) == end {
+                            p2 = visit_n + 1;
+                            break 'pork;
+                        }
+                        p2_start.push_back((ny, nx, v, visit_n + 1));
+                        best2.insert((ny, nx), visit_n + 1);
+                    }
+                }
+            }
+        }
+    }
     println!("Part One: {}", p1);
+    println!("Part Two: {}", p2);
 }
