7d8c8174fa012ce8f9c1d6fdd15d54ece6897239
ChuckleScriptCraft <hxzzmxnch@gmail.com>
2025-12-08 23:07:59 +0000

    Day 13 2022 - Part Two

--- 2022/13/rust_202213/src/main.rs
+++ 2022/13/rust_202213/src/main.rs
@@ -29,6 +29,7 @@ fn main() {
 
     let file = read_to_string("2022\\13\\input.txt").unwrap();
     let lines: Vec<&str> = file.lines().into_iter().filter(|x| x.len() > 0).collect();
+
     for i in (0..lines.len()).step_by(2) {
         let left: Packet = serde_json::from_str(lines[i]).unwrap();
         let right: Packet = serde_json::from_str(lines[i + 1]).unwrap();
@@ -36,5 +37,26 @@ fn main() {
             p1 += (i / 2) as i32 + 1;
         }
     }
-    println!("Part One: {}", p1)
+
+    // P2
+    let d2: Packet = serde_json::from_str("[[2]]").unwrap();
+    let d6: Packet = serde_json::from_str("[[6]]").unwrap();
+
+    let mut lines: Vec<Packet> = lines
+        .iter()
+        .map(|x| serde_json::from_str(x).unwrap())
+        .collect();
+
+    lines.push(d2.clone());
+    lines.push(d6.clone());
+    lines.sort();
+
+    let p2: usize = lines
+        .iter()
+        .enumerate()
+        .filter(|(_i, packet)| **packet == d2 || **packet == d6)
+        .map(|(i, _)| i + 1)
+        .product();
+    println!("Part One: {}", p1);
+    println!("Part One: {}", p2);
 }
