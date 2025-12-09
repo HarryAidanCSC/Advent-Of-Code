8f33e1f6b85f764c2e36c4caf6b15b36e9e7456b
ChuckleScriptCraft <hxzzmxnch@gmail.com>
2025-12-09 21:00:22 +0000

    Day 15 2022 - Part Two

--- 2022/15/rust_202215/src/main.rs
+++ 2022/15/rust_202215/src/main.rs
@@ -15,7 +15,7 @@ fn main() {
         .collect();
 
     let beacons: HashSet<(i32, i32)> = lines.iter().map(|c| (c.2, c.3)).collect();
-
+    let mut p2_lines: Vec<(i32, i32, i32)> = vec![];
     let mut p1: HashSet<i32> = HashSet::new();
     for (x0, y0, x1, y1) in lines {
         let w = x0.max(x1) - x0.min(x1);
@@ -23,17 +23,41 @@ fn main() {
         let mh = w + h;
 
         let gap = mh - y0.abs_diff(2000000) as i32;
-        if x0 == 8 && y0 == 7 {
-            println!("{}", mh);
-        }
         if gap >= 0 {
             for i in 0..=gap {
                 p1.insert(x0 - i);
                 p1.insert(x0 + i);
             }
         }
+        p2_lines.push((x0, y0, mh));
     }
 
     p1.retain(|x| !beacons.contains(&(*x, 2000000)));
+    println!("{:?}", p2_lines);
+    // P2
+    let mut p2: i64 = 0;
+    'beef: for row in 0..4_000_000 {
+        let mut ranges = vec![];
+
+        for (x, y, mh) in p2_lines.iter() {
+            let gap = mh - y.abs_diff(row) as i32;
+            if gap >= 0 {
+                ranges.push((x - gap, x + gap));
+            }
+        }
+
+        ranges.sort();
+        let mut cur_max = ranges[0].1;
+
+        for (start, end) in ranges[1..].iter() {
+            if *start > cur_max + 1 {
+                p2 = ((cur_max as i64 + 1) * 4_000_000) + row as i64;
+                break 'beef;
+            }
+            cur_max = cur_max.max(*end);
+        }
+    }
+
     println!("Part One: {}", p1.len());
+    println!("Part Two: {}", p2)
 }
