341f1873ffc8a40f0616848701e5c5b357f5e1af
ChuckleScriptCraft <hxzzmxnch@gmail.com>
2025-12-09 00:13:51 +0000

    Day 14 2022 - Part One

---
+++ 2022/14/rust_202214/src/main.rs
@@ -0,0 +1,59 @@
+use std::collections::{HashMap, HashSet};
+use std::fs::read_to_string;
+fn main() {
+    let file = read_to_string("../../input.txt").unwrap();
+    let lines: Vec<Vec<(usize, usize)>> = file
+        .lines()
+        .map(|line| {
+            line.split(" -> ")
+                .map(|c| {
+                    let (x, y) = c.split_once(',').unwrap();
+                    (x.parse::<usize>().unwrap(), y.parse::<usize>().unwrap())
+                })
+                .collect()
+        })
+        .collect();
+
+    let mut maxy = 0;
+    // Occupado
+    let mut occupado: HashSet<(usize, usize)> = HashSet::new();
+    for line in lines {
+        let (mut x, mut y) = (line[0].0, line[0].1);
+        maxy = maxy.max(y);
+        for coords in &line[1..] {
+            for dx in x.min(coords.0)..=x.max(coords.0) {
+                for dy in y.min(coords.1)..=y.max(coords.1) {
+                    occupado.insert((dx, dy));
+                }
+            }
+            x = coords.0;
+            y = coords.1;
+            maxy = maxy.max(y);
+        }
+    }
+
+    // Kick off
+    let mut p1 = 0;
+    loop {
+        p1 += 1;
+        let (mut x, mut y) = (500, 0);
+        // Drop the y
+        while y <= maxy {
+            y += 1;
+            if !occupado.contains(&(x, y)) {
+            } else if !occupado.contains(&(x - 1, y)) {
+                x -= 1;
+            } else if !occupado.contains(&(x + 1, y)) {
+                x += 1;
+            } else {
+                occupado.insert((x, y - 1));
+                y -= 1;
+                break;
+            }
+        }
+        if y == maxy + 1 {
+            break;
+        }
+    }
+    println!("Part One: {}", p1 - 1);
+}
