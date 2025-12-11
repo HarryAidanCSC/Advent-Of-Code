2fd46d25b7925ce12d464ca989900c305e4015d7
ChuckleScriptCraft <hxzzmxnch@gmail.com>
2025-12-11 11:36:55 +0000

    Day 11 2025 - Part Two

--- 2025/11/rust_202511/src/main.rs
+++ 2025/11/rust_202511/src/main.rs
@@ -2,7 +2,6 @@ use std::collections::HashMap;
 use std::fs::read_to_string;
 
 fn dfs(nodes: &HashMap<&str, Vec<&str>>, cur_node: &str, goal_node: &str, p1: &mut u64) {
-    // BC
     if cur_node == goal_node {
         *p1 += 1;
         return;
@@ -14,6 +13,32 @@ fn dfs(nodes: &HashMap<&str, Vec<&str>>, cur_node: &str, goal_node: &str, p1: &m
     return;
 }
 
+fn dfs2<'a>(
+    nodes: &HashMap<&str, Vec<&'a str>>,
+    cur_node: &'a str,
+    goal_node: &str,
+    fft: bool,
+    dac: bool,
+    cache: &mut HashMap<(&'a str, bool, bool), u64>,
+) -> u64 {
+    if cur_node == goal_node {
+        return if fft && dac { 1 } else { 0 };
+    }
+
+    let state = (cur_node, fft, dac);
+    if cache.contains_key(&state) {
+        return *cache.get(&state).unwrap();
+    }
+    let mut paths = 0;
+
+    for ne_node in nodes.get(cur_node).unwrap().iter() {
+        let nfft = fft || *ne_node == "fft";
+        let ndac = dac || *ne_node == "dac";
+        paths += dfs2(&nodes, ne_node, "out", nfft, ndac, cache)
+    }
+    cache.insert(state, paths);
+    return paths;
+}
 fn main() {
     let file = read_to_string("../../input.txt").unwrap();
     let lines: Vec<&str> = file.lines().collect();
@@ -28,5 +53,10 @@ fn main() {
 
     let mut p1: u64 = 0;
     dfs(&nodes, "you", "out", &mut p1);
+
+    let mut cache = HashMap::new();
+    let p2 = dfs2(&nodes, "svr", "out", false, false, &mut cache);
+
     println!("Part One: {}", p1);
+    println!("Part Two: {}", p2);
 }
